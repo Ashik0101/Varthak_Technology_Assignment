4f65304c9331c0a220b87bea4a3738549bc3fc0e
Md Ashik Ansari <aashiqmohd04@gmail.com>
2023-11-07 10:24:26 +0530

    created the feature to get the books created by 10 minutes ago or more and less than 10 min ago

--- src/controllers/BookController.ts
+++ src/controllers/BookController.ts
@@ -40,9 +40,48 @@ export const createBook = async (req: Request, res: Response) => {
 };
 
 //Controller to view the books of a particular user
+// export const viewOwnBook = async (req: Request, res: Response) => {
+//   try {
+//     const books = await Book.find({ user: req.body.user }, { user: 0, __v: 0 });
+//     res.status(200).json(books);
+//   } catch (error: any) {
+//     res.status(500).json({
+//       message: "Internal server error",
+//       error: error.message,
+//     });
+//   }
+// };
+
+// Controller to view the books of a particular user
 export const viewOwnBook = async (req: Request, res: Response) => {
   try {
-    const books = await Book.find({ user: req.body.user }, { user: 0, __v: 0 });
+    let books: IBook[] = [];
+
+    if (req.query.old === "1") {
+      // Calculate the date 10 minutes ago
+      const tenMinutesAgo = new Date();
+      tenMinutesAgo.setMinutes(tenMinutesAgo.getMinutes() - 10);
+
+      // Find books created 10 minutes ago or more for the specific user
+      books = await Book.find({
+        user: req.body.user,
+        createdAt: { $lte: tenMinutesAgo },
+      });
+    } else if (req.query.new === "1") {
+      // Calculate the date 10 minutes ago
+      const tenMinutesAgo = new Date();
+      tenMinutesAgo.setMinutes(tenMinutesAgo.getMinutes() - 10);
+
+      // Find books created less than 10 minutes ago for the specific user
+      books = await Book.find({
+        user: req.body.user,
+        createdAt: { $gt: tenMinutesAgo },
+      });
+    } else {
+      // If no query parameter is provided
+      books = await Book.find({ user: req.body.user });
+    }
+
     res.status(200).json(books);
   } catch (error: any) {
     res.status(500).json({
@@ -55,7 +94,29 @@ export const viewOwnBook = async (req: Request, res: Response) => {
 //Controller to see all the created books
 export const viewAllBooks = async (req: Request, res: Response) => {
   try {
-    const books = await Book.find({});
+    let books: IBook[] = [];
+
+    if (req.query.old === "1") {
+      const tenMinutesAgo = new Date();
+      tenMinutesAgo.setMinutes(tenMinutesAgo.getMinutes() - 10);
+
+      //Find books created 10 minutes ago or more
+      books = await Book.find({
+        createdAt: { $lte: tenMinutesAgo },
+      });
+    } else if (req.query.new === "1") {
+      const tenMinutesAgo = new Date();
+      tenMinutesAgo.setMinutes(tenMinutesAgo.getMinutes() - 10);
+
+      // Find books created less than 10 minutes ago
+      books = await Book.find({
+        createdAt: { $gt: tenMinutesAgo },
+      });
+    } else {
+      // If no query parameter is provided
+      books = await Book.find({});
+    }
+
     res.status(200).json(books);
   } catch (error: any) {
     res.status(500).json({
